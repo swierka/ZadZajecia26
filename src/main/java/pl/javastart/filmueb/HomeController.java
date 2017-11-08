95cee07ecea56f8cc68995c93a3b2a65a872ae50
Aleksander <jsdevalx@gmail.com>
2017-11-08 19:31:25 +0100

    zmiany pkt 3 i 4

--- src/main/java/pl/javastart/filmueb/HomeController.java
+++ src/main/java/pl/javastart/filmueb/HomeController.java
@@ -1,14 +1,25 @@
 package pl.javastart.filmueb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    FilmRepository filmRepository;
+
     @GetMapping("/")
-    public String home() {
+    public String filmDetails(Model model) {
+        List<Movie> allMovies = filmRepository.getAllFilms();
+        model.addAttribute("lista", allMovies);
         return "index";
+        //return "/";
     }
 
 }
